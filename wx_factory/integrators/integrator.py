aff81eb3f81dcd840dc3d9432fc1783705379694
Kate <kate.2lviv@gmail.com>
2026-05-28 22:14:01 +0000

    remove prints

--- wx_factory/integrators/integrator.py
+++ wx_factory/integrators/integrator.py
@@ -97,7 +97,7 @@ class Integrator(ABC):
             solver_info = self.solver_info if self.solver_info is not None else SolverInfo()
 
             rhs_times = None
-            print("no timing")
+            # print("no timing")
             
             # if hasattr(self, "rhs") and isinstance(self.rhs, RHS):
             #     self.rhs.retrieve_last_times()
